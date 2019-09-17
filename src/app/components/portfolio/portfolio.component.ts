3e549a108406f93b9387ac0ec6db4accf10de0a4
Nathan Pickard <nathan.pickard87@gmail.com>
2019-09-17 14:50:39 -0700

    Adding animations to portfolio component

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -3,14 +3,43 @@ import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
 import { Portfolio } from 'src/app/services/portfolio.model';
 
+import { trigger, transition, animate, style, query, stagger, animateChild } from '@angular/animations';
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  animations: [
+    trigger('list', [
+      transition(':enter', [
+        query('@items', stagger(60, animateChild()))
+      ])
+    ]),
+    trigger('items', [
+      transition(':enter', [
+        style({ transform: 'scale(0)', opacity: 0 }),
+        animate('0.350s cubic-bezier(.8,-0.6, 0.2, 1.5)',
+          style({ transform: 'scale(1)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class PortfolioComponent implements OnInit {
 
-  selectedType: 'all' | 'Angular' | 'React' | 'Vue' = 'all';
+  types: string[];
+
+  private _selectedType = 'all';
+
+  get selectedType() {
+    return this._selectedType;
+  }
+
+  set selectedType(newValue: string) {
+    if (newValue !== this._selectedType) {
+      this._selectedType = newValue;
+      this.loadPortfolios(this._selectedType);
+    }
+  }
 
   rooms = ['Room 1', 'Living Room'];
 
@@ -19,8 +48,13 @@ export class PortfolioComponent implements OnInit {
   constructor(private portfolioService: PortfolioService) { }
 
   ngOnInit() {
+    this.loadPortfolios(this._selectedType);
+  }
+
+  loadPortfolios(selectedType: string): void {
     this.portfolioService.get().subscribe(data => {
-      this.portfolios = data;
+      this.types = data.map(p => p.type).filter((value, index, self) => self.indexOf(value) === index);
+      this.portfolios = data.filter(p => p.type === selectedType || selectedType === 'all');
     });
   }
 
